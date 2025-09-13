5f1c58f4a3b56734ac87a10cd00538f6856cdb10
Shubhamgoyalbs <786shubhamgoyal@gmail.com>
2025-09-13 14:51:03 +0000

    formatting db

--- backend/db.js
+++ backend/db.js
@@ -1,51 +1,53 @@
 const {Schema, default: mongoose} = require("mongoose")
-const users = new Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-        },
-        phone: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        walletAddress: {
-            type: String,
-            unique: true,
-        },
-        location: {
-            type: String,
-            default: null,
-        },
-        balance: {
-            type: Schema.Types.Decimal128,
-            default: 0.0,
-            min: 0.0,
-        },
-        lockedBalance: {
-            type: Schema.Types.Decimal128,
-            default: 0.0,
-            min: 0.0,
-        },
-        profileCompleted: {type: Boolean,
-        default: false}
+const users = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
-    { timestamps: true }
-)
+    password: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    walletAddress: {
+        type: String,
+        unique: true,
+    },
+    location: {
+        type: String,
+        default: null,
+    },
+    balance: {
+        type: Schema.Types.Decimal128,
+        default: 0.0,
+        min: 0.0,
+    },
+    lockedBalance: {
+        type: Schema.Types.Decimal128,
+        default: 0.0,
+        min: 0.0,
+    },
+    profileCompleted: {
+        type: Boolean,
+        default: false
+    }
+},
+{ 
+    timestamps: true 
+})
 const userModel = mongoose.model('users',users)
 module.exports = {
     userModel
