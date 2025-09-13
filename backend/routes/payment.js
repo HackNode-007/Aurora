ad57acee09eb3e96652cd4ef3d325c30ae63a37f
Shubhamgoyalbs <786shubhamgoyal@gmail.com>
2025-09-13 14:48:31 +0000

    adding payment endpoints but some left now

---
+++ backend/routes/payment.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const paymentRouter = express.Router();
+
+paymentRouter.get("/balance", (req, res) => {
+  const {userId} = req.user;
+  if(!userId){
+    return res.status(404).json({
+      message: "userId not found"
+    })
+  }
+  try{
+    const user = userModel.findById(userId)
+    if(!user){
+      return res.status(405).json({
+        message: "user not found"
+      })
+    }
+    return res.status(200).json({
+      message: "user balance fetched correctly",
+      balance: user.balance
+    })
+  }catch(e){
+    return res.status(500).json({
+      message: "Server error please try again later",
+      error: e.message
+    })
+  }
+})
+
+paymentRouter.post("/payout", (req, res) => {
+  return res.send({
+    message: "Not completed yet"
+  })
+})
+
+paymentRouter.post("/donate", (req, res) => {
+  return res.send({
+    message: "Not completed yet"
+  })
+})
+
+module.exports = {
+  paymentRouter
+}
